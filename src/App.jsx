32d6b0c06773d5c70b7ba34c1d0c0befb5e12286
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-11 00:51:30 +0530

    calculateWinner logic added

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,38 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+console.log(squares);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if(winner) {
+      status = `Winner: ${winner}`;
+  } else {
+      status = "Next Player " + (xIsNext ? "X" : "O");
+  }
 
   const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
     // const nextSquares = [...squares]
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
 
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -46,4 +68,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
