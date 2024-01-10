fd07f43f550c5e41037420a874f55305e483f48d
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-11 00:15:58 +0530

    lift up the state from square to board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 /* Game
        --> Board
-            -->Square 
+            -->Square value="1"
 
        -->History */
 
-function Square() {
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-600 h-12 w-12 m-1 leading-9 text-lg">
-      X
+    <button
+      className="bg-white border border-gray-600 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
+      {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+    // const nextSquares = [...squares]
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
+
   return (
     <>
-     <div>
-      <Square/>
-      <Square/>
-      <Square/>
-     </div>
-     <div>
-      <Square/>
-      <Square/>
-      <Square/>
-     </div>
-     <div>
-      <Square/>
-      <Square/>
-      <Square/>
-     </div>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
   );
 }
